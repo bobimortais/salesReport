119a80fb75489f43eb3b7bed4be2c6f0cb1147e0
bobimortais <robsonzacarias@hotmail.com>
2020-02-11 01:47:16 -0300

    Add write output file and move processed file tasks

--- sales/src/main/java/com/test/sales/app/SalesReportApp.java
+++ sales/src/main/java/com/test/sales/app/SalesReportApp.java
@@ -1,6 +1,8 @@
 package com.test.sales.app;
 
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.nio.file.*;
 
 public class SalesReportApp
@@ -36,31 +38,59 @@ public class SalesReportApp
             try
             {
                 key = watcher.take();
+                for (WatchEvent<?> event : key.pollEvents())
+                {
+                    WatchEvent.Kind<?> kind = event.kind();
+                    WatchEvent<Path> ev = (WatchEvent<Path>) event;
+                    Path fileName = ev.context();
+
+                    System.out.println(kind.name() + ": " + fileName);
+
+                    if (kind == StandardWatchEventKinds.ENTRY_CREATE)
+                    {
+                        moveProcessedFile(fileName);
+                        System.out.println("Novo Arquivo Criado");
+                        writeOutputFile();
+                    }
+                }
+
+                boolean valid = key.reset();
+                if (!valid)
+                {
+                    break;
+                }
             }
-            catch (InterruptedException ex)
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+            catch (InterruptedException e)
             {
                 return;
             }
-
-            for (WatchEvent<?> event : key.pollEvents())
-            {
-                WatchEvent.Kind<?> kind = event.kind();
-                WatchEvent<Path> ev = (WatchEvent<Path>) event;
-                Path fileName = ev.context();
-
-                System.out.println(kind.name() + ": " + fileName);
-
-                if (kind == StandardWatchEventKinds.ENTRY_CREATE)
-                {
-                    System.out.println("Novo Arquivo Criado");
-                }
-            }
-
-            boolean valid = key.reset();
-            if (!valid)
-            {
-                break;
-            }
         }
+    }
+
+    private static void handleFileCreation()
+    {
+
+    }
+
+    private static void moveProcessedFile(Path processedFile) throws IOException
+    {
+        String envVarValue = System.getenv("HOMEPATH");
+        String pathToMove = envVarValue + "/data/prd/" + processedFile;
+        String pathToRead = envVarValue + "/data/in/" + processedFile;
+        Files.move(Paths.get(pathToRead), Paths.get(pathToMove));
+    }
+
+    private static void writeOutputFile() throws IOException
+    {
+        String envVarValue = System.getenv("HOMEPATH");
+        String pathToWrite = envVarValue + "/data/out";
+        FileWriter fileWriter = new FileWriter(pathToWrite + "/robson.txt");
+        PrintWriter printWriter = new PrintWriter(fileWriter);
+        printWriter.print("Teste arquivo gerado");
+        printWriter.close();
     }
 }
